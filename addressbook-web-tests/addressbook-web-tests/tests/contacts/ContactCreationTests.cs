5d35b341799aee01ba8ca9939df09190b11080fc
Kozlova N.I <kozlovani@gmail.com>
2018-07-08 21:12:22 +0500

    Added random data for contact and group tests

--- addressbook-web-tests/addressbook-web-tests/tests/contacts/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/contacts/ContactCreationTests.cs
@@ -6,12 +6,22 @@ namespace WebAddressbookTests
     [TestFixture]
     public class ContactCreationTests : AuthTestBase
     {
-        [Test]
-        public void ContactCreationTest()
+        public static IEnumerable<ContactData> RandomContactDataProvider()
         {
-            //preparate
-            ContactData contact = new ContactData("Ivan", "Ivanov");
-            contact.MiddleName = "Ivanovich";
+            List<ContactData> contacts = new List<ContactData>();
+            for (int i = 0; i < 5; i++)
+            {
+                contacts.Add(new ContactData(GenerateRandomString(30), GenerateRandomString(30))
+                {
+                    Address = GenerateRandomString(100),
+                    Email = GenerateRandomString(100)
+                });
+            }
+            return contacts;
+        }
+        [Test, TestCaseSource("RandomContactDataProvider")]
+        public void ContactCreationTest(ContactData contact)
+        {
             List<ContactData> oldContacts = app.Contacts.GetContactList();
 
             //action
@@ -23,9 +33,7 @@ namespace WebAddressbookTests
             oldContacts.Add(contact);
             oldContacts.Sort();
             newContacts.Sort();
-            Assert.AreEqual(oldContacts, newContacts);
-
-            
+            Assert.AreEqual(oldContacts, newContacts); 
             
         }
 
