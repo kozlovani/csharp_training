fff640a700fff02b6ac82482bea7b784cf5a1005
Nataliya K <kozlovani@gmail.com>
2018-07-12 17:08:28 +0500

    Added test-data-generator to file

--- addressbook-web-tests/addressbook-web-tests/tests/groups/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/groups/GroupCreationTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WebAddressbookTests
 {
@@ -19,7 +20,24 @@ namespace WebAddressbookTests
             }
             return groups;
         }
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
+                });
+            };
+            return groups;
+        }
+
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreationTest(GroupData group)
         {
             //preparate
